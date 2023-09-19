151a94b29d25122edcbfb6051e216fd46c77b539
Justin Bertrand <52991734+jpbaz28@users.noreply.github.com>
2023-09-18 17:59:32 -0700

    added base for level 3 and game icon

--- main_menu.gd
+++ main_menu.gd
@@ -3,7 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Options/StartBtn.grab_focus()
+
+	if !OS.has_feature("pc"):
+		$Options/FullscreenBtn.hide()
+		$Options/QuitBtnBtn.hide()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +18,14 @@ func _ready():
 
 func _on_start_btn_pressed():
 	get_tree().change_scene_to_file("res://level_1.tscn")
+
+
+func _on_fullscreen_btn_pressed():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
